9d5c17308231537c73a2bf664b241079ac32c370
Anastasiya <nastiy232007@yandex.ru>
2025-10-23 14:54:17 +0300

    chore: remove main.py from repository

--- main.py
+++
@@ -1,8 +0,0 @@
-# Lemesheva Anastasiy, 36-a koqorta - Финальный проект. Инженер по тестированию плюс
-def something_wrong_here(a, b):
-    if a == b:
-       print('Тест пройден')
-    else:
-       print('Тест провален')
-              
-something_wrong_here("C", "C")
